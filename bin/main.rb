9f0c7c6911e5a34fac9c6578549576590ebd3272
Deepakdanger <depk.96.kumar@gmail.com>
2021-03-29 16:12:38 +0530

    Define array in logic

--- bin/main.rb
+++ bin/main.rb
@@ -13,57 +13,57 @@ def display_board(board)
   puts s
 end
 
-# puts 'Welcome to Tic-Tac-Toe v1.0'
-# puts 'Press Enter to continue_'
-# blank = gets.chomp
-# puts 'Welcome, player 1'
-# puts 'Please input your game name...'
-# p1 = gets.chomp.upcase
-# puts
-# puts 'Welcome, player 2'
-# puts 'Please input your game name...'
-# p2 = gets.chomp.upcase
-# puts
-# puts "#{p1} plays with 'X' and #{p2} plays with 'O'"
-# puts
-# puts 'INSTRUCTION!!!:Input the corresponding cell number in which you want to place your piece when prompted'
-# puts
-# puts 'Let us get right to it!'
-# puts
+puts 'Welcome to Tic-Tac-Toe v1.0'
+puts 'Press Enter to continue_'
+blank = gets.chomp
+puts 'Welcome, player 1'
+puts 'Please input your game name...'
+p1 = gets.chomp.upcase
+puts
+puts 'Welcome, player 2'
+puts 'Please input your game name...'
+p2 = gets.chomp.upcase
+puts
+puts "#{p1} plays with 'X' and #{p2} plays with 'O'"
+puts
+puts 'INSTRUCTION!!!:Input the corresponding cell number in which you want to place your piece when prompted'
+puts
+puts 'Let us get right to it!'
+puts
 puts
 display_board(board)
 puts
 puts
 
-# def cell_check(cell, select1, arr)
-#   if arr.include? cell
-#     puts 'Cell position has been taken, select a new cell'
-#     select1
-#   elsif (1..9).include? cell
-#     arr << cell
-#     true
+def cell_check(cell, select1, arr)
+  if arr.include? cell
+    puts 'Cell position has been taken, select a new cell'
+    select1
+  elsif (1..9).include? cell
+    arr << cell
+    true
 
-#   else
-#     puts 'Invalid selection, Please enter a valid number between 1-9'
-#     select1
-#   end
-# end
+  else
+    puts 'Invalid selection, Please enter a valid number between 1-9'
+    select1
+  end
+end
 
-# arr = []
-# session = true
+arr = []
+session = true
 
-# while session
-#   puts 'Player 1: Please choose a cell number '
-#   select1 = false
-#   while select1 == false
-#     cell = gets.chomp.to_i
-#     select1 = cell_check(cell, select1, arr)
-#   end
+while session
+  puts "#{p1}: Please choose a cell number "
+  select1 = false
+  while select1 == false
+    cell = gets.chomp.to_i
+    select1 = cell_check(cell, select1, arr)
+  end
 
-#   puts 'Player 2: Please choose a cell number '
-#   select1 = false
-#   while select1 == false
-#     cell = gets.chomp.to_i
-#     select1 = cell_check(cell, select1, arr)
-#   end
-# end
+  puts "#{p2}: Please choose a cell number "
+  select1 = false
+  while select1 == false
+    cell = gets.chomp.to_i
+    select1 = cell_check(cell, select1, arr)
+  end
+end
