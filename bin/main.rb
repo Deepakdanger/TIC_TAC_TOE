0c167003ee6be3bff121aa40d90f4a3289493fb6
Deepakdanger <depk.96.kumar@gmail.com>
2021-03-29 15:53:34 +0530

    Arrange the sequence

--- bin/main.rb
+++ bin/main.rb
@@ -1,66 +1,65 @@
 #!/usr/bin/env ruby
 
 board = %w[1 2 3 4 5 6 7 8 9]
-  
+
 def display_board(board)
-  s= "#{board[0]} | #{board[1]} | #{board[2]}\n
+  s = "#{board[0]} | #{board[1]} | #{board[2]}\n
       #{board[3]} | #{board[4]} | #{board[5]}\n
       #{board[6]} | #{board[7]} | #{board[8]}\n"
   puts s
 end
 
-puts "Welcome to Tic-Tac-Toe v1.0"
-puts "Press Enter to continue_"
+puts 'Welcome to Tic-Tac-Toe v1.0'
+puts 'Press Enter to continue_'
 blank = gets.chomp
-puts "Welcome, player 1"
-puts "Please input your game name..."
+puts 'Welcome, player 1'
+puts 'Please input your game name...'
 p1 = gets.chomp.upcase
-puts 
-puts "Welcome, player 2"
-puts "Please input your game name..."
+puts
+puts 'Welcome, player 2'
+puts 'Please input your game name...'
 p2 = gets.chomp.upcase
-puts 
+puts
 puts "#{p1} plays with 'X' and #{p2} plays with 'O'"
 puts
-puts "INSTRUCTION!!!:Input the corresponding cell number in which you want to place your piece when prompted"
+puts 'INSTRUCTION!!!:Input the corresponding cell number in which you want to place your piece when prompted'
 puts
-puts "Let us get right to it!"
+puts 'Let us get right to it!'
 puts
 puts
 display_board(board)
 puts
 puts
 
-def cell_check(cell,select1,arr)
+def cell_check(cell, select1, arr)
   if arr.include? cell
-    puts "Cell position has been taken, select a new cell"    
-    return select1
+    puts 'Cell position has been taken, select a new cell'
+    select1
   elsif (1..9).include? cell
     arr << cell
-    select1= true    
-    return select1
+    true
+
   else
-    puts "Invalid selection, Please enter a valid number between 1-9"    
-    return select1
+    puts 'Invalid selection, Please enter a valid number between 1-9'
+    select1
   end
 end
 
-arr=[]
+arr = []
 session = true
 
-while session  
-  puts "Player 1: Please choose a cell number "  
-  select1= false
-  while select1==false
+while session
+  puts 'Player 1: Please choose a cell number '
+  select1 = false
+  while select1 == false
     cell = gets.chomp.to_i
-    select1 = cell_check(cell,select1,arr)
+    select1 = cell_check(cell, select1, arr)
   end
 
-  puts "Player 2: Please choose a cell number "  
-  select1= false
-  while select1==false
+  puts 'Player 2: Please choose a cell number '
+  select1 = false
+  while select1 == false
     cell = gets.chomp.to_i
-    select1= cell_check(cell,select1,arr)
+    select1 = cell_check(cell, select1, arr)
   end
-end 
-
+end
