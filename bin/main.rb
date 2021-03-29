943c07379a0054ee8b89bfcd782bac7f2d3ab05f
Deepakdanger <depk.96.kumar@gmail.com>
2021-03-29 15:48:57 +0530

    Add the input sequence

--- bin/main.rb
+++ bin/main.rb
@@ -3,7 +3,9 @@
 board = %w[1 2 3 4 5 6 7 8 9]
   
 def display_board(board)
-  s= "#{board[0]} | #{board[1]} | #{board[2]}\n#{board[3]} | #{board[4]} | #{board[5]}\n#{board[6]} | #{board[7]} | #{board[8]}\n"
+  s= "#{board[0]} | #{board[1]} | #{board[2]}\n
+      #{board[3]} | #{board[4]} | #{board[5]}\n
+      #{board[6]} | #{board[7]} | #{board[8]}\n"
   puts s
 end
 
@@ -28,25 +30,37 @@ puts
 display_board(board)
 puts
 puts
+
+def cell_check(cell,select1,arr)
+  if arr.include? cell
+    puts "Cell position has been taken, select a new cell"    
+    return select1
+  elsif (1..9).include? cell
+    arr << cell
+    select1= true    
+    return select1
+  else
+    puts "Invalid selection, Please enter a valid number between 1-9"    
+    return select1
+  end
+end
+
+arr=[]
 session = true
-while session
-  puts "Player 1: Please choose a cell number "
-  cell = gets.chomp.to_i
-  if cell > 9 || cell <= 0
-    puts "Invalid selection, Please enter a valid number between 1-9"
+
+while session  
+  puts "Player 1: Please choose a cell number "  
+  select1= false
+  while select1==false
     cell = gets.chomp.to_i
-  elsif cell.position_taken?
-    puts "Cell position has been taken, select a new cell"
-  end 
-
-
-  puts "Player 2: Please choose a cell"
-  cell = gets.chomp.to_i
-  if cell > 9 || cell <= 0
-    puts "Invalid selection, Please enter a valid number between 1-9"
-  elsif cell.position_taken?
-    puts "Cell position has been taken, select a new cell"
+    select1 = cell_check(cell,select1,arr)
   end
 
+  puts "Player 2: Please choose a cell number "  
+  select1= false
+  while select1==false
+    cell = gets.chomp.to_i
+    select1= cell_check(cell,select1,arr)
+  end
 end 
 
