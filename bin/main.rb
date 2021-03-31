e59b55d0d5385dceece6de771c06a1ddcf030128
Deepakdanger <depk.96.kumar@gmail.com>
2021-03-31 14:51:51 +0530

    Do the logic main part

--- bin/main.rb
+++ bin/main.rb
@@ -1,6 +1,5 @@
 #!/usr/bin/env ruby
-require_relative "../lib/logic"
-
+require_relative '../lib/logic'
 
 board = %w[1 2 3 4 5 6 7 8 9]
 
@@ -53,8 +52,8 @@ puts
 puts
 puts
 
-def cell_check(cell,person, arr1,arr2)
-  if (arr1.include? cell) || (arr2.include? cell )
+def cell_check(cell, person, arr1, arr2)
+  if (arr1.include? cell) || (arr2.include? cell)
     puts 'Cell position has been taken, select a new cell'
     false
   elsif (1..9).include? cell
@@ -69,56 +68,54 @@ def cell_check(cell,person, arr1,arr2)
     false
   end
 end
-param2=[]
+param2 = []
 arr1 = []
 arr2 = []
 session = true
 turn = 0
 while session
   checklist = Logic.new
-    
-    puts "#{@p1}: Please choose a cell number "
-    turn+=1
-    select1 = false
-    while select1 == false
-      cell = gets.chomp.to_i
-      select1 = cell_check(cell, @p1, arr1,arr2)      
-    end    
-    board[cell-1]='X'
-    display_board(board)
-    param1=checklist.get_param1(arr1)
-    
-    winner=checklist.check_win(param1, param2, @p1)
-    
-    if winner ==true
-      p "GAME OVER"
-      break
-    end
 
-    if turn==9
-      puts "GAME IS TIE"
-      puts "GAME OVER"
-      break
-    end
+  puts "#{@p1}: Please choose a cell number "
+  turn += 1
+  select1 = false
+  while select1 == false
+    cell = gets.chomp.to_i
+    select1 = cell_check(cell, @p1, arr1, arr2)
+  end
+  board[cell - 1] = 'X'
+  display_board(board)
+  param1 = checklist.get_param1(arr1)
 
-    puts "#{@p2}: Please choose a cell number "
-    turn+=1
-    select1 = false
-    while select1 == false
-      cell = gets.chomp.to_i
-      select1 = cell_check(cell, @p2, arr1,arr2)      
-    end
-    board[cell-1]='O'
-    display_board(board)
-    param2=checklist.get_param2(arr2)
-    
-    winner=checklist.check_win(param1, param2, @p2)
-    
-    if winner ==true
-      p "GAME OVER"
-      break
-    end    
-    
-    
- 
+  winner = checklist.check_win(param1, param2, @p1)
+
+  if winner == true
+    p 'GAME OVER'
+    break
+  end
+
+  if turn == 9
+    puts 'GAME IS TIE'
+    puts 'GAME OVER'
+    break
+  end
+
+  puts "#{@p2}: Please choose a cell number "
+  turn += 1
+  select1 = false
+  while select1 == false
+    cell = gets.chomp.to_i
+    select1 = cell_check(cell, @p2, arr1, arr2)
+  end
+  board[cell - 1] = 'O'
+  display_board(board)
+  param2 = checklist.get_param2(arr2)
+
+  winner = checklist.check_win(param1, param2, @p2)
+
+  if winner == true
+    p 'GAME OVER'
+    break
+  end
+
 end
--- lib/logic.rb
+++ lib/logic.rb
@@ -1,28 +1,25 @@
 class Logic
   def win
-    win = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[3,5,7],[1,5,9]]
+    [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [3, 5, 7], [1, 5, 9]]
   end
 
   def get_param1(arr1)
-    param1 = win.map {|a| a.intersection(arr1.sort)}.select {|a| a.length == 3}
-    return param1
+    win.map { |a| a.intersection(arr1.sort) }.select { |a| a.length == 3 }
   end
 
   def get_param2(arr2)
-    param2 = win.map {|a| a.intersection(arr2.sort)}.select {|a| a.length == 3}
-    return param2
+    win.map { |a| a.intersection(arr2.sort) }.select { |a| a.length == 3 }
   end
 
-  def check_win (param1, param2,per)
+  def check_win(param1, param2, per)
     if win.include? param1.flatten
-      puts"#{per} wins"
-      return true      
+      puts "#{per} wins"
+      true
     elsif win.include? param2.flatten
       puts "#{per} wins"
-      return true     
+      true
     else
-      return false
+      false
     end
   end
-
 end
