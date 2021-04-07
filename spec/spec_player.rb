fb74ea07646e11890e70140265e4bb56f6be84ef
Deepakdanger <depk.96.kumar@gmail.com>
2021-04-07 13:38:51 +0530

    Add player rspec

---
+++ spec/spec_player.rb
@@ -0,0 +1,19 @@
+require_relative '../lib/player'
+require 'rspec'
+include RSpec
+
+describe Player do
+    player=Player.new
+    describe "name_check method " do
+        let(:per){ '' }
+        it "should return false is person1 length = 0" do
+            expect(player.name_check(per)).not_to be true
+        end
+    end
+    describe "name_check method " do
+        let(:per){ 'DEEP' }
+        it "should accept player length > 0" do
+            expect(player.name_check(per)).to be true
+        end
+    end
+end
