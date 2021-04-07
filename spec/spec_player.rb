2d444de13148d2c78fb3942c4d70cb59183a01b1
Deepakdanger <depk.96.kumar@gmail.com>
2021-04-07 13:46:23 +0530

    Change one parameter

--- spec/spec_player.rb
+++ spec/spec_player.rb
@@ -1,19 +1,19 @@
+include RSpec
 require_relative '../lib/player'
 require 'rspec'
-include RSpec
 
 describe Player do
-    player=Player.new
-    describe "name_check method " do
-        let(:per){ '' }
-        it "should return false is person1 length = 0" do
-            expect(player.name_check(per)).not_to be true
-        end
+  player = Player.new
+  describe 'name_check method ' do
+    let(:per) { '' }
+    it 'should return false is person1 length = 0' do
+      expect(player.name_check(per)).not_to be true
     end
-    describe "name_check method " do
-        let(:per){ 'DEEP' }
-        it "should accept player length > 0" do
-            expect(player.name_check(per)).to be true
-        end
+  end
+  describe 'name_check method ' do
+    let(:per) { 'DEEP' }
+    it 'should accept player length > 0' do
+      expect(player.name_check(per)).to be true
     end
+  end
 end
